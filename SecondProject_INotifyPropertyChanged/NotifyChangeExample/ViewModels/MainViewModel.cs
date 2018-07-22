562a5cfa1627b4de8f1f345b2b39619675ce0862
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-07-22 12:57:51 +0530

    addition of MainViewModel to tie all viewModels togeather to present as one to the form for binding.

---
+++ SecondProject_INotifyPropertyChanged/NotifyChangeExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace NotifyChangeExample.ViewModels
+{
+    public class MainViewModel
+    {
+        public PersonViewModel Person { get; private set; }
+
+        public BackgroundViewModel Background { get; private set; }
+
+        public MainViewModel()
+        {
+            Person = new PersonViewModel();
+            Background = new BackgroundViewModel();
+        }
+
+        // Person binding will be one way to source.
+        public void SetBackground(Brush brushColor)
+        {
+            Background.Color = brushColor;
+        }
+    }
+}
