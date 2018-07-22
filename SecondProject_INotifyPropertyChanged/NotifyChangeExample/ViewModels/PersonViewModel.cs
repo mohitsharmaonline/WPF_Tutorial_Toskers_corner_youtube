a35d3195a95e82662a9f6a3feec7a865e11e9d0b
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-07-22 12:35:49 +0530

    addition of PersonViewModel to act as data provider for Name textbox of the form.

---
+++ SecondProject_INotifyPropertyChanged/NotifyChangeExample/ViewModels/PersonViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyChangeExample.ViewModels
+{
+    public class PersonViewModel : ObservableObject
+    {
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                // we need to make sure that if _name is nothing
+                // we return something, because it will be one way binding
+                // and we would not like to break binding by
+                // throwing an error.
+                if(string.IsNullOrWhiteSpace(_name))
+                {
+                    return "Unknown";
+                }
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+
+
+        }
+    }
+}
