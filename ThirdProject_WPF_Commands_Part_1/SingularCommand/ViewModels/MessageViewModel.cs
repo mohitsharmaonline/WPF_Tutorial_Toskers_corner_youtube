2c107cd165f809a9264836399120d2503dbe3bfb
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-07-25 20:37:30 +0530

    Initial files having viewmodel and command raw code.

---
+++ ThirdProject_WPF_Commands_Part_1/SingularCommand/ViewModels/MessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SingularCommand.ViewModels
+{
+    public class MessageViewModel
+    {
+        public string MessageText { get; set; }
+
+        public MessageViewModel()
+        {
+
+        }
+
+        public void DisplayMessage()
+        {
+            MessageBox.Show(MessageText);
+        }
+    }
+}
