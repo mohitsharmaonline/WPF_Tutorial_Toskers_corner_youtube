e826e4a6625a33e3393565f5b424bf070f29bef6
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-07-28 14:42:01 +0530

    Initial command file and declaration of plan of action.

---
+++ FourthProject_WPF_Commands_Part_2/RelayCommand/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RelayCommand
+{
+    // we are going to add an Action here as before and also something known as the predicate.
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
