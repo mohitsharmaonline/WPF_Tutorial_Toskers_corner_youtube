ec9f67b5e7cb73afcd1d344ff4aeb18d203a5f3a
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-15 21:23:43 +0530

    Setting up of all view models with minor code cleanup of unnecessary usings, as well as some comments to tell what we are trying to implement here. UI bindings and design is still pending.

---
+++ EightProject_WPF_RelativeSource_And_Xaml_Debugging/VisualTreeSample/ListViewViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace VisualTreeSample
+{
+    public class ListViewViewModel
+    {
+        public ObservableCollection<string> Names { get; set; }
+
+        public ListViewViewModel()
+        {
+            Names = new ObservableCollection<string>()
+            {
+                "Joe",
+                "Jane",
+                "John",
+                "Jessica",
+                "Matt"
+            };
+        }
+    }
+}
