8751dfda44ebfe564db20a24bdf965d3e32ed007
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-15 21:08:41 +0530

    Initial addition of previously made classes to be useful for our this example.

---
+++ EightProject_WPF_RelativeSource_And_Xaml_Debugging/VisualTreeSample/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace VisualTreeSample
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
