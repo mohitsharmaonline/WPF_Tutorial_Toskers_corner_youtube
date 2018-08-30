6dcbc790542829c7e933865950d83aba242dd8aa
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-30 21:19:04 +0530

    Initial implementation of IValueConverter.

--- NinthProject_WPF_IValueConverter_Part_1/ValueConverters/Converters/BoolToVisibilityConverter.cs
+++ NinthProject_WPF_IValueConverter_Part_1/ValueConverters/Converters/BoolToVisibilityConverter.cs
@@ -1,7 +1,25 @@
 ﻿
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
 namespace ValueConverters.Converters
 {
-    public class BoolToVisibilityConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
+        /********************************************************************************
+         * At beginner level we will first care about parameter of type object which
+         * we shall be passing. this 'value' contains the value we are interested in 
+         * conversion.
+         * ****************************************************************************/
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
