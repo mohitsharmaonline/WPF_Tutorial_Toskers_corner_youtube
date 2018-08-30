0033fdfcb83dd75cc86e0f9e2d03cc28fcc15018
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-30 20:48:16 +0530

    Initial project with running code, which would be used to display the problem that should be solved with IValueConverter.

---
+++ NinthProject_WPF_IValueConverter_Part_1/NonValueConverter/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Windows;
+
+namespace NonValueConverter
+{
+    public class MainViewModel: ObservableObject
+    {
+        private bool isVisible;
+        public bool IsVisible
+        {
+            get
+            {
+                return isVisible;
+            }
+            set
+            {
+                isVisible = value;
+                UpdateVisibility(value);
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
+
+        private Visibility visibility;
+        public Visibility Visibility
+        {
+            get
+            {
+                return visibility;
+            }
+            set
+            {
+                visibility = value;
+                OnPropertyChanged(nameof(Visibility));
+            }
+        }
+
+        public MainViewModel()
+        {
+            isVisible = true;
+            Visibility = Visibility.Visible;
+        }
+
+        private void UpdateVisibility(bool isVisible)
+        {
+            if(isVisible)
+            {
+                Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ NinthProject_WPF_IValueConverter_Part_1/NonValueConverter/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel;
+
+namespace NonValueConverter
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
