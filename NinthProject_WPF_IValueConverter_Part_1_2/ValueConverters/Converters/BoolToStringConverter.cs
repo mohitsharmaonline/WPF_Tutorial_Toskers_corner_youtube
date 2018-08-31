68eba4a845b7a72aa20395055972c4aa7de6aa00
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-31 20:47:25 +0530

    Addition of BoolToStringConverter class

---
+++ NinthProject_WPF_IValueConverter_Part_1_2/ValueConverters/Converters/BoolToStringConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ValueConverters.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var answerBool = (bool)value;
+
+            if (answerBool)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var answerString = (string)value;
+
+            if(answerString.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
